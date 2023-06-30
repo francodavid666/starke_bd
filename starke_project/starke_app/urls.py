aef13c9b297ed32e3f8a1bd70beafd6350387fb1
david-repo <david_franco@outlook.com.ar>
2023-06-30 14:41:40 -0300

    l

--- starke_project/starke_app/urls.py
+++ starke_project/starke_app/urls.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from .views import * 
+from django.conf.urls.static import static
+from django.urls import re_path
+from django.conf import settings
+from django.views.static import serve
+
 
 
 
--- starke_project/starke_project/urls.py
+++ starke_project/starke_project/urls.py
@@ -14,11 +14,23 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path,include
+
 from django.conf import settings
 from django.conf.urls.static import static
+from django.urls import re_path
+from django.views.static import serve
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include ('starke_app.urls')),
 ]+ static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+
+
+urlpatterns += [                    #serve
+    re_path(r'^media/(?P<path>.*)$', serve, {
+        'document_root': settings.MEDIA_ROOT,
+    }),
+]
+
